c734abe5af2d0cfe664a303f131b3d9864c4a72e
Matteo De Leonardis <matteo.deleonardis2@gmail.com>
2022-04-16 13:29:47 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.26

---
+++ src/PottsProductionScheduling.jl
@@ -0,0 +1,5 @@
+module PottsProductionScheduling
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using PottsProductionScheduling
+using Test
+
+@testset "PottsProductionScheduling.jl" begin
+    # Write your tests here.
+end
