31557b5c7de80905460610999e608219e8e726ba
Matteo De Leonardis <matteo.deleonardis2@gmail.com>
2022-04-17 19:08:06 +0200

    fixed cost function

--- src/common.jl
+++ src/common.jl
@@ -1,5 +1,6 @@
-function step_cost(env, agent, t)
-    return sum([env.h[t, s] for s ∈ agent.σ[t, :]]) + sum([env.A[m, t, agent.σ[m, t], agent.σ[m, t+1]] for m ∈ 1:env.M]) +
-        sum([env.J[m, n, t, agent.σ[t, m], agent.σ[t, n]] for m ∈ 1:env.M, n ∈ 1:env.M])
+function step_cost(env, σt1, σt2, t)
+    return sum([env.h[t, s] for s ∈ σt1]) + 
+    sum([env.A[m, t, σt1[m], σt2[m]] for m ∈ 1:env.M]) +
+        sum([sum([env.J[m, n, t, σt1[m], σt1[n]] for n ∈ m+1:env.M]) for m ∈ 1:env.M-1])
 end
 
