0e032bb5b332c510cead3901b87249e18f6f4324
Matteo De Leonardis <matteo.deleonardis2@gmail.com>
2022-05-17 11:55:46 +0200

    fixed bug in recursion

--- src/optimize.jl
+++ src/optimize.jl
@@ -9,7 +9,7 @@ function optimize(env)
     #recursion steps
     for t in 2:env.T-1
         for i in 1:action_space
-            R[t, i], traceback_actions[t, i] = findmin([step_cost(env, int2vec(x, env.N, env.M), int2vec(i, env.N, env.M), t) + R[t, x]
+            R[t, i], traceback_actions[t, i] = findmin([step_cost(env, int2vec(x, env.N, env.M), int2vec(i, env.N, env.M), t) + R[t-1, x]
                 for x in 1:action_space])
         end
     end
@@ -20,7 +20,7 @@ function optimize(env)
     for t in env.T:-1:2
         optimal_actions[t-1] = traceback_actions[t-1, optimal_actions[t]]
     end
-    return optimal_cost, map(x->int2vec(x, env.N, env.M), optimal_actions)
+    return optimal_cost, map(x->int2vec(x, env.N, env.M), optimal_actions), R
 end
 
     
