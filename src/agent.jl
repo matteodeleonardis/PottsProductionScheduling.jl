b5897b301271dc7ec257af6bc3a8afb54d20f934
Matteo De Leonardis <matteo.deleonardis2@gmail.com>
2022-05-17 11:57:09 +0200

    minor fix

--- src/agent.jl
+++ src/agent.jl
@@ -2,7 +2,7 @@ struct Agent
     T::Int #time horizon
     M::Int #number of machines
     N::Int #number of goods
-    σ::Matrix{Int} #actions σ[m, t]  ∈ {1,...,N}
+    σ::Matrix{Int} #actions σ[t, m]  ∈ {1,...,N}
 end
 
 function Agent(s, n)
