ebe7d56223e747b9e4b4957c3d56cee13a6b6aff
Matteo De Leonardis <matteo.deleonardis2@gmail.com>
2022-05-17 11:59:03 +0200

    added new exports

--- src/PottsProductionScheduling.jl
+++ src/PottsProductionScheduling.jl
@@ -5,7 +5,7 @@ module PottsProductionScheduling
     include("optimize.jl")
     include("utils.jl")
 
-    export Environment, Agent, step_cost, optimize
+    export Environment, Agent, step_cost, optimize, cost
 
     #exporting only for debugging
     export base10_conv, baseN_conv, vec2int, int2vec
