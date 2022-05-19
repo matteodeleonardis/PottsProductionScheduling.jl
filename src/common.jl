a41d9c162138c6bc770fb772398c67655e876b87
Matteo De Leonardis <matteo.deleonardis2@gmail.com>
2022-05-19 15:38:13 +0200

    clean up in cost function

--- src/common.jl
+++ src/common.jl
@@ -1,9 +1,10 @@
-function step_cost(env, demand, σt1, σt2, t)
-    
-    return sum([env.h[t, s] for s ∈ σt1]) + 
-    sum([env.A[m, t, σt1[m], σt2[m]] for m ∈ 1:env.M]) +
-        sum([sum([env.J[m, n, t, σt1[m], σt1[n]] for n ∈ m+1:env.M]) for m ∈ 1:env.M-1]) +
-        sum(env.I[t,:] .* expected_storage(demand, σt1))
+function production_step_cost(env, σt1, σt2, t)
+    return sum([env.h[t, s] for s ∈ σt1]) + sum([env.A[m, t, σt1[m], σt2[m]] for m ∈ 1:env.M]) +
+        sum([sum([env.J[m, n, t, σt1[m], σt1[n]] for n ∈ m+1:env.M]) for m ∈ 1:env.M-1]) 
+end
+
+function step_cost(env, demand, previous_storage, σt1, σt2, t)
+    production_step_cost(env, σt1, σt2) + sum(env.I[t,:] .* (previous_storage .+ expected_storage(demand, σt1)))
 end
 
 
