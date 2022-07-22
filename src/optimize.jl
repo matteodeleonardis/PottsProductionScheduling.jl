299ad39d518c37932b3f7dea27d3474f7f0da122
Matteo De Leonardis <matteo.deleonardis2@gmail.com>
2022-07-22 18:05:16 +0200

    simulated annealing fixed

--- src/optimize.jl
+++ src/optimize.jl
@@ -1,38 +1,30 @@
-function mc_step!(env, agent, demand, machine, time)
+function mc_step!(env, agent, demand, machine, time, β)
+    agent_tmp = deepcopy(agent)
     r = rand(collect(1:env.N))
-    set_action!(agent, r, machine, time)
+    set_action!(agent_tmp, r, machine, time)
+    Δcost = expected_total_cost(env, agent_tmp, demand) - expected_total_cost(env, agent, demand)
+    η = rand()
+    if η <= exp(-β*Δcost)
+        set_action!(agent, r, machine, time)
+    end
     return nothing
 end
 
 
-function mc_sweep!(env, agent, demand)
+function mc_sweep!(env, agent, demand, β)
     for m ∈ 1:env.M
         for t ∈ 1:env.T
-            mc_step!(env, agent, demand, m, t)
+            mc_step!(env, agent, demand, m, t, β)
         end
     end
     return expected_total_cost(env, agent, demand)
 end
 
 
-function annealing!(env, agent, demand, β)
-    agent_old = deepcopy(agent)
-    cost = expected_total_cost(env, agent, demand)
-    Δcost = mc_sweep!(env, agent, demand) - cost
-    r = rand()
-    if exp(β*Δcost) <= r
-        return agent
-    end
-    return agent_old
-end
-
-
-function annealing_schedule!(env, agent, demand, βschedule)
-    ag = deepcopy(agent)
+function annealing!(env, agent, demand, βschedule)
     costs = zeros(length(βschedule))
     for (i,β) ∈ pairs(βschedule)
-        ag = annealing!(env, ag, demand, β)
-        costs[i] = expected_total_cost(env, ag, demand)
+        costs[i] = mc_sweep!(env, agent, demand, β)
     end
-    return ag, costs
+    return costs
 end
